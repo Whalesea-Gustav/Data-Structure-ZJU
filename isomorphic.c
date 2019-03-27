09de0b99795a6bc1ca4a0f56f910277887730de8
Whalesea-Gustav <asipirinofjim@gmail.com>
2019-03-27 10:37:20 +0800

    changes on json

--- isomorphic.c
+++ isomorphic.c
@@ -12,27 +12,29 @@ struct TreeNode {
     Tree Right;
 } T1[MAXTREE], T2[MAXTREE];
 
-Tree BuildTree(TreeNode tree[]) {
+
+Tree BuildTree(struct TreeNode tree[]) {
     int N;
-    scanf("%d", &N);
-    if (N == 0) return Null;
+    scanf("%d\n", &N);
+    if (N == 0) return Null; //边界情况
     int isRoot[N];
+
     for (int i = 0; i< N; i++) isRoot[i] = 1;
 
     for (int i = 0; i < N; i++) {
         char data, cl, cr;
-        scanf("%c %c %c", &tree[i].Data, &cl, &cr); // 如果有特殊字符出现，需要以char形式导入以判断
+        scanf("%c %c %c\n", &tree[i].Data, &cl, &cr); // 如果有特殊字符出现，需要以char形式导入以判断
         if (cl == '-') {
             tree[i].Left = Null;
         } else {
             tree[i].Left = cl-'0';
-            isRoot[tree[i].Left] = 0;
+            isRoot[cl-'0'] = 0;
         }
         if (cr == '-') {
             tree[i].Right = Null;
         } else {
             tree[i].Right = cr - '0';
-            isRoot[tree[i].Right] = 0;
+            isRoot[cr-'0'] = 0;
         }
     }
     for (int i = 0; i < N; i++) {
@@ -41,7 +43,8 @@ Tree BuildTree(TreeNode tree[]) {
 }
 
 int main() {
-    printf("hello world\n");
-    //Tree Root1 = BuildTree(T1);
+    printf("hello world2\n");
+    Tree Root1 = BuildTree(T1);
     //Tree Root2 = BuildTree(T2);
+    printf("Root1:%d\n", Root1);
 }
